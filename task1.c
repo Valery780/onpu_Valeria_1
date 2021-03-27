b3b361a6bf2f2b8d975e83aeb079a57178a4f6de
Valery780 <70340887+Valery780@users.noreply.github.com>
2021-03-27 17:12:55 +0200

    Update task1.c

--- task1.c
+++ task1.c
@@ -1,15 +1,33 @@
-//Посчитать объем конуса Македонская Валерия УП-202
-#include<stdio.h>
-#include<math.h>
+//Посчитать объем конуса Македонская Валерия УП-202, работа с массивами
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <stdlib.h>
 
-int main()
+void main()
 {
     float V;
     float h;
     float R;
+    float aver;
     int n;
+    double * arr;
+    void output(double * arr, int n)
+    
+    {
+        int i;
+        for (i = 0; i < n; i++)
+        {
+          print("arr[%d] = %lf \n",i, arr[i]);
+        }   
+    }
+        
+   
     printf("Сколько раз необходимо посчитать объем конуса? ");
     scanf("%d", &n);
+
+    arr = malloc(n*sizeof(double));
+
     for (int i = 0; i < n; i++)
     {
         printf("Введите высоту конуса h: ");
@@ -21,5 +39,17 @@ int main()
         V = 1.0/3.0*M_PI*pow(R,2)*h;
         printf("Объем конуса равняется %f\n", V);
     }
-    return 0;
+    printf("История расчетов: \n");
+    output(arr, n);
+
+    sum = 0;
+     for (int i = 0; i < n; i++){
+        aver = arr[i];
+        sum = sum + aver;
+        }
+        aver = sum/n;
+        printf ("Среднее значение объема: %lf \n", aver);
+        free(arr);
+
+return 0;
 }
